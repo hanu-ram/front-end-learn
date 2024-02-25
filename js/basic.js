fb9bce7b6a6b6638ab1ca103f391cf799a4413d4
hanu-ram <hanumanthhanuram17@gmail.com>
2024-02-25 15:20:42 +0530

    javascript practise

---
+++ js/basic.js
@@ -0,0 +1,13 @@
+console.log( 2 + 4 );
+let myName = 'Hanu';
+var stringTemplate = `Hello ${ myName }`;
+console.log( stringTemplate );
+function respond ()
+{
+  alert( "Greeting Message: " + stringTemplate )
+}
+
+if ( myName != 'hanu' )
+{
+  console.log( true )
+}
