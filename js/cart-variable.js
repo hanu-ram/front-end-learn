75d63072aa09daadad47ac3bdb051fa4abafe51e
hanu-ram <hanumanthhanuram17@gmail.com>
2024-02-25 18:06:19 +0530

    cart.js

--- js/cart-variable.js
+++ js/cart-variable.js
@@ -7,19 +7,19 @@ function showQty ()
 
 function addToCart ()
 {
-  qty = qty + 1;
+  qty += 1;
   showQty();
 }
 
 function add2 ()
 {
-  qty = qty + 2;
+  qty += 2;
   showQty();
 }
 
 function add3 ()
 {
-  qty = qty + 3;
+  qty += 3;
   showQty();
 }
 
